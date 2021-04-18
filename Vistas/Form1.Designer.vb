f6adec407f911d189c216013bd4b61a056c88cad
AnaBruno2 <apu2194@gmail.com>
2021-04-17 21:00:57 -0300

    quinto commit

--- Vistas/Form1.Designer.vb
+++ Vistas/Form1.Designer.vb
@@ -22,9 +22,41 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnAceptar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(71, 176)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 0
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.Location = New System.Drawing.Point(197, 176)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAceptar.TabIndex = 1
+        Me.btnAceptar.Text = "aceotar"
+        Me.btnAceptar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents btnSalir As System.Windows.Forms.Button
+    Friend WithEvents btnAceptar As System.Windows.Forms.Button
 
 End Class
--- Vistas/Form1.vb
+++ Vistas/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
 
+    Private Sub btnSalir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSalir.Click
+
+    End Sub
 End Class
